c5d4fe283eabedd09eb53c9ee4fbac4be371696e
Mobius3610 <amcorpstein@outlook.com>
2023-08-28 09:37:34 -0500

    Bug Fix: Fixes game crash when mouse pointer would leave the game map zone.

--- game_map.py
+++ game_map.py
@@ -73,7 +73,7 @@ class GameMap:
 
 	def in_bounds(self, x:int, y:int) -> bool:
 		# if char is in bounds rtn True
-		return 0 <= x <= self.width and 0 <= y <= self.height
+		return 0 <= x < self.width and 0 <= y < self.height
 
 	def render(self, console: Console) -> None:
 		console.tiles_rgb[0 : self.width, 0 : self.height] = np.select(
